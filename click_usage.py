7b25e85cf523db700633733fe32fc3313c3223bd
kTurek95 <k.turek1995@gmail.com>
2023-10-20 20:46:19 +0100

    added initial menu function to click_usage module

---
+++ click_usage.py
@@ -0,0 +1,9 @@
+def menu():
+    """
+    Display a simple menu with options for the user.
+    """
+    print('---------- MENU ----------')
+    print('1. Enter your email data and save it to a file')
+    print('2. Display configuration from a file')
+    print('3. Connect to email')
+    print('4. Exit')
