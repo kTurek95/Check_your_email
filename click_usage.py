76b859533a2d0d2795288840b431ac8830c30528
kTurek95 <k.turek1995@gmail.com>
2023-10-21 15:11:22 +0100

    Add mail_connection function for displaying email configurations and establishing connections

--- click_usage.py
+++ click_usage.py
@@ -7,3 +7,18 @@ def menu():
     print('2. Display configuration from a file')
     print('3. Connect to email')
     print('4. Exit')
+
+
+def mail_connection(config, client):
+    """
+    Displays available email server configurations from a file and allows the user
+    to select one of them. Upon selecting a configuration, the function establishes
+    a connection to the email server and searches for messages.
+    """
+    i = 1
+    for configuration in config.get_info_from_file():
+        print(f'{i}. {configuration}')
+        i += 1
+    user_config = int(input('Wybierz konfigurację: '))
+    imap_server = client.connect_with_server(user_config)
+    client.search_messages(imap_server)
